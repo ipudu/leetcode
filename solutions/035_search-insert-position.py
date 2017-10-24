31ab7ee3e1a299cc336cbda89e57da3ab77dad12
Pu Du <rocketsboy@gmail.com>
2017-10-23 20:36:55 -0700

    search insert position

---
+++ solutions/035_search-insert-position.py
@@ -0,0 +1,31 @@
+"""
+Given a sorted array and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+"""
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        i = 0
+        j = len(nums)
+        while i < j:
+            if target <= nums[i]:
+                return i
+            else:
+                i += 1
+            if target > nums[j-1]:
+                return j
+            else:
+                j -= 1
+        return i
