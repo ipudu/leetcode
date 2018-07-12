57bad68fd24e4227953357057b6661bd60e82540
Pu Du <rocketsboy@gmail.com>
2018-07-12 00:35:27 -0500

    to lower case

---
+++ solutions/709_to-lower-case.py
@@ -0,0 +1,11 @@
+"""
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+"""
+
+class Solution:
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
