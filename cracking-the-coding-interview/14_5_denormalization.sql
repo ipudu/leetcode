55fa14e36c2863057976abe637d3593cc2775f51
Pu Du <rocketsboy@gmail.com>
2019-03-07 16:00:39 -0600

    denormalization

---
+++ cracking-the-coding-interview/14_5_denormalization.sql
@@ -0,0 +1,13 @@
+/*
+denormalization is a database optimization technique in which we add redundant
+data to one or more tables.
+
+cons:
+updates and inserts are more expensive.
+data may be inconsistent.
+require more storage.
+
+pros:
+retrieving data is faster.
+queires to retrieve can be simpler.
+*/
