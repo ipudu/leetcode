e1bf810f72b299e5b209c2a880e5f23ca6b1ab92
Pu Du <rocketsboy@gmail.com>
2019-12-29 12:04:32 -0600

    correct the answer

--- cracking-the-coding-interview/1_4_palindrome-permutation.py
+++ cracking-the-coding-interview/1_4_palindrome-permutation.py
@@ -1,8 +1,24 @@
 #solution 1
 #O(n)
 
-def palindrome_permutation(a, b):
-    return ''.join(sorted(list(a))) == ''.join(sorted(list(b)))
+def palindrome_permutation(s):
+    d = {}
+    for c in s:
+        if c != ' ':
+            if c in d:
+                d[c] += 1
+            else:
+                d[c] = 1
+    count = 0
+
+    for k, v in d.items():
+        if v % 2 == 1:
+            count += 1
+    if count > 1:
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
-    print(palindrome_permutation('taco cat', 'atco cta'))
+    print(palindrome_permutation('taco cat'))
+    print(palindrome_permutation('Hello World!'))
